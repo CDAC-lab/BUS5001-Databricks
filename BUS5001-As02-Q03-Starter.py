e6d6a7e09495cfa0af2f666fbed60f6010859061
nmills <nishan.mills@gmail.com>
2023-05-16 13:28:06 +1000

    BUS5001As02Q3 Starter Template
    Starter template to help you choose the appropriate dataset to use for the question.

---
+++ BUS5001-As02-Q03-Starter.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # BUS5001 - Assignment 02 - Question 03 - Starter
+# MAGIC 
+# MAGIC Use the delta tables that have been provided to answer your questions.
+# MAGIC 
+# MAGIC ## 1. Allocated Year
+# MAGIC We have preprocessed delta tables for years 2012 through to 2022. For assignment 2 question 3, the year of data you have to work with will be determined by the last digit of your student index. As an example, if your student index ends with a 1, you will choose year 2021, if your student index ends with a 0 you will choose year 2020.
+# MAGIC 
+# MAGIC ## 2. Preprocessed Delta Table
+# MAGIC The preprocessed delta table data file locations are in the format; "/data/silver/taxi/nyc-yellow/{YYYY}"
